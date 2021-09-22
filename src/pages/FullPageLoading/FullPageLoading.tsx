63360ec587cbe5ba1cbf24403a84e83e643f43ed
Ny Aina Erick <ainaerik@gmail.com>
2021-09-22 21:09:47 +0300

    Improve full page loading

--- src/pages/FullPageLoading/FullPageLoading.tsx
+++ src/pages/FullPageLoading/FullPageLoading.tsx
@@ -1,8 +1,12 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
+import { useTheme } from '@mui/material/styles'
 import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import CircularProgress from '@mui/material/CircularProgress'
 
 import useStyles from './styles'
+import HelmetHeader from '@components/HelmetHeader'
 
 interface FullPageLoadingProps {}
 
@@ -11,11 +15,24 @@ interface FullPageLoadingProps {}
  * @param props FullPageLoadingProps
  * @returns
  */
-const FullPageLoading: FC<FullPageLoadingProps> = (props) => {
+const FullPageLoading: FC<FullPageLoadingProps> = ({}) => {
   const { t } = useTranslation('common')
   const classes = useStyles()
+  const theme = useTheme()
 
-  return <Box className={classes.root}>{t('pages.fullpageLoading.text')}</Box>
+  return (
+    <>
+      <HelmetHeader title={t('pages.fullpageLoading.text')} />
+      <Box className={classes.root}>
+        <Box textAlign="center">
+          <CircularProgress sx={{ my: 2, fontSize: 32 }} />
+          <Typography variant="subtitle1" color={theme.palette.text.primary}>
+            {t('pages.fullpageLoading.text')}
+          </Typography>
+        </Box>
+      </Box>
+    </>
+  )
 }
 
 export default FullPageLoading
